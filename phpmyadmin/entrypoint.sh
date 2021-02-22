b3276e092d52232a1914c65957af0604e8264607
Michal Sotolar <michal@sotolar.com>
2021-02-22 21:29:01 +0100

    phpmyadmin: 5.0.4

---
+++ phpmyadmin/entrypoint.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+if [ ! -f /etc/phpmyadmin/config.secret.inc.php ]; then
+   cat > /etc/phpmyadmin/config.secret.inc.php <<EOT
+<?php
+\$cfg['blowfish_secret'] = '$(tr -dc 'a-zA-Z0-9~!@#$%^&*_()+}{?></";.,[]=-' < /dev/urandom | fold -w 32 | head -n 1)';
+EOT
+fi
+
+if [ ! -f /etc/phpmyadmin/config.user.inc.php ]; then
+    touch /etc/phpmyadmin/config.user.inc.php
+fi
+
+if [ ! -z "${HIDE_PHP_VERSION}" ]; then
+    echo "PHP version is now hidden."
+    echo -e 'expose_php = Off\n' > $PHP_INI_DIR/conf.d/phpmyadmin-hide-php-version.ini
+fi
+
+if [ ! -z "${PMA_CONFIG_BASE64}" ]; then
+    echo "Adding the custom config.inc.php from base64."
+    echo "${PMA_CONFIG_BASE64}" | base64 -d > /etc/phpmyadmin/config.inc.php
+fi
+
+if [ ! -z "${PMA_USER_CONFIG_BASE64}" ]; then
+    echo "Adding the custom config.user.inc.php from base64."
+    echo "${PMA_USER_CONFIG_BASE64}" | base64 -d > /etc/phpmyadmin/config.user.inc.php
+fi
+
+rsync -a --delete /usr/src/phpmyadmin/ /var/www/html
+
+get_docker_secret() {
+    local env_var="${1}"
+    local env_var_file="${env_var}_FILE"
+    if [[ -n "${!env_var_file}" ]]; then
+        export "${env_var}"="$(cat "${!env_var_file}")"
+    fi
+}
+
+get_docker_secret PMA_PASSWORD
+get_docker_secret MYSQL_ROOT_PASSWORD
+get_docker_secret MYSQL_PASSWORD
+get_docker_secret PMA_HOSTS
+get_docker_secret PMA_HOST
+get_docker_secret PMA_CONTROLPASS
+
+exec "$@"
