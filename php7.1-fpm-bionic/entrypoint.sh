5cbbeea6bac1e2f204fab0760d3d7eec867b0f0f
Michal Sotolar <michal@sotolar.com>
2021-03-07 14:46:11 +0100

    php7.1-fpm-bionic: sudo NOPASSWD

--- php7.1-fpm-bionic/entrypoint.sh
+++ php7.1-fpm-bionic/entrypoint.sh
@@ -22,6 +22,7 @@ fi
 
 if ! id -u "$PHP_USER" > /dev/null 2>&1; then
     adduser --quiet --disabled-password --uid "$PHP_USER_UID" --shell /bin/bash --home /home/"$PHP_USER" --gecos "$PHP_USER_GECOS" "$PHP_USER"
+    echo "$PHP_USER ALL=(ALL) NOPASSWD: ALL" | tee /etc/sudoers.d/"$PHP_USER"
     touch /home/"$PHP_USER"/.sudo_as_admin_successful
     usermod -aG dialout,disk,video,sudo "$PHP_USER"
 fi
