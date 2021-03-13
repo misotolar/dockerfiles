ca8003201f5e46e83ab7a051e33c3dc078c04507
Michal Sotolar <michal@sotolar.com>
2021-03-13 11:26:10 +0100

    geoipupdate: xtables build

--- geoipupdate/entrypoint.sh
+++ geoipupdate/entrypoint.sh
@@ -3,6 +3,13 @@
 envsubst < "/etc/GeoIP.conf.docker" > "/etc/GeoIP.conf"
 
 echo "${GEOIPUPDATE_SCHEDULE} /usr/local/bin/geoipupdate -v -f /etc/GeoIP.conf" > /etc/crontabs/geoipupdate
+if [ ! -z "${GEOIPUPDATE_XTABLES_SCHEDULE}" ]; then
+    echo "${GEOIPUPDATE_XTABLES_SCHEDULE} /usr/local/bin/xt-update.sh" >> /etc/crontabs/geoipupdate
+fi
+
 /usr/local/bin/geoipupdate -v -f /etc/GeoIP.conf
+if [ ! -z "${GEOIPUPDATE_XTABLES_SCHEDULE}" ]; then
+    /usr/local/bin/xt-update.sh
+fi
 
 exec "$@"
---
+++ geoipupdate/xtables/xt-update.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+TMPDIR=$(mktemp -d)
+
+curl -fsSL -o "${TMPDIR}"/geolite.zip https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country-CSV\&license_key="${GEOIPUPDATE_LICENSE_KEY}"\&suffix=zip
+unzip -q "${TMPDIR}"/geolite.zip -d "${TMPDIR}"
+
+cd "${TMPDIR}"/GeoLite2-Country-CSV_*
+xt-build.pl -D "${GEOIPUPDATE_XT_DIR:-"/usr/share/xt_geoip"}"
+
+rm -rf "${TMPDIR}"
+exit 0
+unzip 
