937786941c5552f330abd86db08c9aafdf939afb
Michal Sotolar <michal@sotolar.com>
2021-06-29 12:14:19 +0200

    fontello: remove config

--- fontello/entrypoint.sh
+++ fontello/entrypoint.sh
@@ -2,12 +2,4 @@
 
 set -e
 
-cp /usr/local/fontello/config/fontello.com.yml.config /usr/local/fontello/config/fontello.com.yml
-
-# replace mount point if env variables are defined
-sed -i "s/mount: http:\/\/fontello.com/mount: ${FONTELLO_PROTO:-https}:\/\/${FONTELLO_HOST:-fontello.com}/" /usr/local/fontello/config/fontello.com.yml
-
-# uncomment `forwarded: true`
-sed -i "s/#forwarded: true/forwarded: true/" /usr/local/fontello/config/fontello.com.yml
-
 exec "$@"
