b0a06209a4ad9c2fb1f696c7427d7bb831e56fe5
Michal Sotolar <michal@sotolar.com>
2021-07-26 15:11:56 +0200

    phppgadmin: cleanup

--- phppgadmin/entrypoint.sh
+++ phppgadmin/entrypoint.sh
@@ -5,7 +5,7 @@ if [ ! -z "${HIDE_PHP_VERSION}" ]; then
     echo -e 'expose_php = Off\n' > $PHP_INI_DIR/conf.d/phppgadmin-hide-php-version.ini
 fi
 
-rsync -a --delete /usr/src/phppgadmin/ /var/www/html
+tar -xf /usr/src/phpPgAdmin.tar.gz -C /var/www/html --exclude-from=/usr/src/phpPgAdmin.exclude --strip-components=1
 ln -fs /etc/phppgadmin/config.inc.php /var/www/html/conf/config.inc.php
 
 exec "$@"
