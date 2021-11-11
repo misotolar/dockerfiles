9b0e7b3b11e8a2ff5eadce1eaed89040b5bd07ae
Michal Sotolar <michal@sotolar.com>
2021-11-11 10:41:03 +0100

    phppgadmin: alpine 3.14

--- phppgadmin/entrypoint.sh
+++ phppgadmin/entrypoint.sh
@@ -1,11 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ ! -z "${HIDE_PHP_VERSION}" ]; then
-    echo "PHP version is now hidden."
-    echo -e 'expose_php = Off\n' > $PHP_INI_DIR/conf.d/phppgadmin-hide-php-version.ini
-fi
-
-tar -xf /usr/src/phpPgAdmin.tar.gz -C /var/www/html --exclude-from=/usr/src/phpPgAdmin.exclude --strip-components=1
-ln -fs /etc/phppgadmin/config.inc.php /var/www/html/conf/config.inc.php
+tar -xf /usr/src/phpPgAdmin.tar.gz -C . --exclude-from=/usr/src/phpPgAdmin.exclude --strip-components=1
+ln -fs /usr/local/etc/phppgadmin/config.inc.php /usr/local/phppgadmin/conf/config.inc.php
 
 exec "$@"
