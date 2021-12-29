8c670b19e7cb0921a90bad2be3dcbe318bb74b47
Michal Sotolar <michal@sotolar.com>
2021-12-29 20:34:44 +0100

    drone-rsync: initial commit

---
+++ drone-rsync/README.md
@@ -0,0 +1,1 @@
+# dockerfiles/drone-rsync
---
+++ drone-rsync/entrypoint.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -e
+
+: "${PLUGIN_PORT:=22}"
+: "${PLUGIN_SOURCE:=./}"
+
+if [[ -z "$PLUGIN_REMOTE" ]]; then
+    prinf "Remote host not set.\n"
+    exit 1
+fi
+
+if [[ -z "$PLUGIN_TARGET" ]]; then
+    print "Remote target not set.\n"
+    exit 1
+fi
+
+if [[ -z "$PLUGIN_USERNAME" ]]; then
+    prinf "Remote user not set.\n"
+    exit 1
+fi
+
+if [[ -z "$PLUGIN_PASSWORD" ]]; then
+    prinf "Remote password not set.\n"
+    exit 1
+fi
+
+if [[ -z "$PLUGIN_ARGS" ]]; then 
+    PLUGIN_ARGS="-az --delete"
+fi
+
+EXPR="rsync $PLUGIN_ARGS"
+EXPR="$EXPR -e 'sshpass -p %s ssh -p %s -o UserKnownHostsFile=/dev/null -o LogLevel=quiet -o StrictHostKeyChecking=no'"
+
+IFS=','; read -ra INCLUDE <<< "$PLUGIN_INCLUDE"
+for include in "${INCLUDE[@]}"; do
+    EXPR="$EXPR --include=$include"
+done
+
+IFS=','; read -ra EXCLUDE <<< "$PLUGIN_EXCLUDE"
+for exclude in "${EXCLUDE[@]}"; do
+    EXPR="$EXPR --exclude=$exclude"
+done
+
+IFS=','; read -ra FILTER <<< "$PLUGIN_FILTER"
+for filter in "${FILTER[@]}"; do
+    EXPR="$EXPR --filter=$filter"
+done
+
+EXPR="$EXPR $PLUGIN_SOURCE"
+
+eval "$(printf "$EXPR" "$PLUGIN_PASSWORD" "$PLUGIN_PORT") $PLUGIN_USERNAME@$PLUGIN_REMOTE:$PLUGIN_TARGET"
