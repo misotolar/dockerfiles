7c9effb99a45e26ed49c9ccdbf0c0a0594e4d1cf
Michal Sotolar <michal@sotolar.com>
2021-12-29 21:05:00 +0100

    makepkg: initial commit

---
+++ makepkg/makepkg.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+if [[ -z "$BUILD_PATH" ]]; then
+    if [[ ! -z "$DRONE_STEP_NAME" ]]; then
+        BUILD_PATH=/drone/src/"$DRONE_STEP_NAME"
+    else
+        prinf "Build path not set.\n"
+        exit 1
+    fi
+fi
+
+if [[ -z "$EXPORT_PATH" ]]; then
+    if [[ ! -z "$DRONE_STEP_NAME" ]]; then
+        EXPORT_PATH=/drone/src/export
+    else
+        EXPORT_PATH=/home/build
+    fi
+fi
+
+if [[ ! -d "$EXPORT_PATH" ]]; then
+    sudo mkdir -p "$EXPORT_PATH"
+    sudo chown "$(stat -c '%u:%g' "$BUILD_PATH"/PKGBUILD)" "$EXPORT_PATH"
+fi
+
+cp -r "$BUILD_PATH" /tmp/build
+cd /tmp/build
+
+paru -U --noconfirm
+sudo chown "$(stat -c '%u:%g' "$BUILD_PATH"/PKGBUILD)" ./*pkg.tar*
+sudo mv ./*.pkg.tar* "$EXPORT_PATH"
