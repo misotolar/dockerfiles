ac0548ca308f1e9f5a2d972893b0cddbdd576642
Michal Sotolar <michal@sotolar.com>
2022-05-13 10:36:11 +0200

    phpmyadmin: 5.2.0

--- phpmyadmin/entrypoint.sh
+++ phpmyadmin/entrypoint.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+if [ ! -d /usr/src/phpMyAdmin ]; then
+    mkdir -p /usr/src/phpMyAdmin
+    tar -xf /usr/src/phpMyAdmin.tar.xz -C /usr/src/phpMyAdmin --exclude-from=/usr/src/phpMyAdmin.exclude --strip-components=1
+fi
+
+if [ ! -e index.php ]; then
+	rsync -rlD --delete /usr/src/phpMyAdmin/ /usr/local/phpmyadmin
+else
+	rsync -rlD --delete --exclude-from /usr/src/phpMyAdmin.exclude /usr/src/phpMyAdmin/ /usr/local/phpmyadmin
+fi
+
 if [ ! -f /etc/phpmyadmin/config.secret.inc.php ]; then
    cat > /etc/phpmyadmin/config.secret.inc.php <<EOT
 <?php
@@ -7,8 +18,7 @@ if [ ! -f /etc/phpmyadmin/config.secret.inc.php ]; then
 EOT
 fi
 
-tar -xf /usr/src/phpMyAdmin.tar.xz -C . --exclude-from=/usr/src/phpMyAdmin.exclude --strip-components=1
-sed -i "s@define('CONFIG_DIR'.*@define('CONFIG_DIR', '/etc/phpmyadmin/');@" /usr/local/phpmyadmin/libraries/vendor_config.php
+sed -i "s@'configFile' => .*@'configFile' => '/etc/phpmyadmin/config.inc.php',@" /usr/local/phpmyadmin/libraries/vendor_config.php
 mkdir -p /usr/local/phpmyadmin/tmp; chown www-data:www-data /usr/local/phpmyadmin/tmp
 
 exec "$@"
