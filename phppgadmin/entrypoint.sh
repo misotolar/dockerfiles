e425b4f6775e1ed2fca84ac149f21a2b29b21728
Michal Sotolar <michal@sotolar.com>
2024-04-03 19:42:35 +0200

    phppgadmin: 7.14.7-mod

--- phppgadmin/entrypoint.sh
+++ phppgadmin/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-tar -xf /usr/src/phpPgAdmin.tar.gz -C . --exclude-from=/usr/src/phpPgAdmin.exclude --strip-components=1
+tar -xf /usr/src/phppgadmin.tar.gz -C . --exclude-from=/usr/src/phppgadmin.exclude --strip-components=1
 ln -fs /usr/local/etc/phppgadmin/config.inc.php /usr/local/phppgadmin/conf/config.inc.php
 
 exec "$@"
