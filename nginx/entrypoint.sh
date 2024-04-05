eac76a804a7dc6076a5f08dd82364a6f2d6c4cbb
Michal Sotolar <michal@sotolar.com>
2024-04-05 02:00:52 +0200

    nginx: certbot-ocsp-fetcher

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,5 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 set -ex
+
+if [[ -d /etc/letsencrypt ]]; then
+    /usr/local/sbin/certbot-ocsp-fetcher --output-dir="$OCSP_RESPONSE_PATH" --no-reload-webserver
+fi
 
 exec "$@"
