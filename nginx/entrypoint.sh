9c50a6a3919a95187a0faf1c4e920400f4db3d32
Michal Sotolar <michal@sotolar.com>
2024-04-18 09:54:52 +0200

    nginx: 1.25.5

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -2,6 +2,12 @@
 
 set -ex
 
+NGINX_UUID="${NGINX_UUID:-82}"
+if ! id -u www-data > /dev/null 2>&1; then
+    adduser -u "$NGINX_UUID" -h /var/www -D -S -G www-data www-data
+fi
+
+OCSP_RESPONSE_PATH="${OCSP_RESPONSE_PATH-/usr/local/nginx/cache/ocsp}"
 if [[ -d /etc/letsencrypt ]]; then
     /usr/local/sbin/certbot-ocsp-fetcher --output-dir="$OCSP_RESPONSE_PATH" --no-reload-webserver
 fi
