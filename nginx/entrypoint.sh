29dc8854390e440bc2b4da5945632e186c068cd6
Michal Sotolar <michal@sotolar.com>
2024-04-21 20:40:48 +0200

    nginx: entrypoint hooks

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -2,6 +2,10 @@
 
 set -ex
 
+find "/usr/local/etc/entrypoint-hooks.d" -maxdepth 1 -iname '*.sh' '(' -type f -o -type l ')' -print | sort | while read -r script; do
+    sh -c "$script"
+done
+
 NGINX_UUID="${NGINX_UUID:-82}"
 if ! id -u www-data > /dev/null 2>&1; then
     adduser -u "$NGINX_UUID" -h /var/www -D -S -G www-data www-data
@@ -11,5 +15,9 @@ OCSP_RESPONSE_PATH="${OCSP_RESPONSE_PATH-/usr/local/nginx/cache/ocsp}"
 if [[ -d /etc/letsencrypt ]]; then
     /usr/local/sbin/certbot-ocsp-fetcher --output-dir="$OCSP_RESPONSE_PATH" --no-reload-webserver
 fi
+
+find "/usr/local/etc/entrypoint-post-hooks.d" -maxdepth 1 -iname '*.sh' '(' -type f -o -type l ')' -print | sort | while read -r script; do
+    sh -c "$script"
+done
 
 exec "$@"
